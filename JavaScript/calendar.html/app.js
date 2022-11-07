736c6489f2a59050b9ea2a9a622ff590eb995625
dev-yun <yunpro2766@gmail.com>
2022-11-07 09:36:59 +0900

    calendar 코드 수정

--- JavaScript/calendar.html/app.js
+++ JavaScript/calendar.html/app.js
@@ -2,10 +2,11 @@ function calendar(newYear, newMonth) {
   const time = new Date(newYear, newMonth - 1, 1);
   // 만약 2022년 11월 30일이라면 new Date(2022, 10, 31)은 1을 반환한다. (30일까지라서 다음달 1일을 반환, 28일까지라면 다음달 3일을 반환)
   // 그래서 32 - 1은 31, 32 - 2는 30, 32 - 4는 28일임을 구할 수 있음
-  // const timeLength = 32 - new Date(newYear, newMonth - 1, 31).getDate();
+  const timeLength = 32 - new Date(newYear, newMonth - 1, 32).getDate();
   //하지만 (year, month, 0)으로 쉽게 구할 수 있음
-  const timeLength = new Date(newYear, newMonth, 0).getDate();
+  // const timeLength = new Date(newYear, newMonth, 0).getDate();
 
+  console.log(timeLength);
   let year = time.getFullYear();
   let month = time.getMonth();
   let date = time.getDate();
