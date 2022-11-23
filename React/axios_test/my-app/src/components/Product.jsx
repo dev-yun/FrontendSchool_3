3977a012f5cef406af3e6f49e73f67358636deda
dev-yun <yunpro2766@gmail.com>
2022-11-23 16:16:38 +0900

    11/23일

---
+++ React/axios_test/my-app/src/components/Product.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Product() {
+  useEffect(() => {
+    axios.get('http://test.api.weniv.co.kr/mall').then((res) => {
+      console.log('axios');
+      console.log(res);
+      console.log(res.data);
+    });
+
+    fetch('http://test.api.weniv.co.kr/mall')
+      .then((res) => {
+        console.log('fetch');
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <div>Product</div>;
+}
+
+export default Product;
